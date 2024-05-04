153eeca144149f46ca84370c8e20473ed07b0383
Armando Sanchez <asan5900@gmail.com>
2024-05-04 14:08:27 -0700

    New Project Added

--- src/pages/CardInfo.jsx
+++ src/pages/CardInfo.jsx
@@ -1,30 +1,40 @@
-import fitness from "../videos/365 Fitness - Google Chrome 2024-02-06 14-04-36.mp4"
-import portfolio from "../videos/Portfolio.mp4"
-import fakeflix from "../videos/FakeFlix.mp4"
-
+import thedonutman from "../videos/thedonutman.mp4";
+import fitness from "../videos/365 Fitness - Google Chrome 2024-02-06 14-04-36.mp4";
+import portfolio from "../videos/Portfolio.mp4";
+import fakeflix from "../videos/FakeFlix.mp4";
 
 export default [
-    {
-        title: "365 Fitness",
-        id: 1,
-        paragraph: "365 Fitness",
-        video: fitness,
-        websiteLink: "https://365ftness.netlify.app/",
-        githubLink: "https://github.com/armando12100/git-github.com-armando12100-Website-Practice"
-    },
-    {
-        title: "Portfolio",
-        id: 2,
-        paragraph: "Portfolio",
-        video: portfolio,
-        websiteLink:"https://armandosportfolio.netlify.app/",
-        githubLink:"https://github.com/armando12100/Portfolio"
-    },{
-        title: "FakeFlix",
-        id: 3,
-        paragraph: "Netflix Clone",
-        video: fakeflix,
-        websiteLink:"https://bucolic-sherbet-380004.netlify.app/",
-        githubLink:"https://github.com/armando12100/netflix_clone"
-    }
-]
+  {
+    title: "The Donut Man Dos",
+    id: 0,
+    paragraph: "The Donut Man Dos",
+    video: thedonutman,
+    websiteLink: "https://thedonutmandos.netlify.app/",
+    githubLink: "https://github.com/armando12100/The-Donut-Man",
+  },
+  {
+    title: "365 Fitness",
+    id: 1,
+    paragraph: "365 Fitness",
+    video: fitness,
+    websiteLink: "https://365ftness.netlify.app/",
+    githubLink:
+      "https://github.com/armando12100/git-github.com-armando12100-Website-Practice",
+  },
+  {
+    title: "Portfolio",
+    id: 2,
+    paragraph: "Portfolio",
+    video: portfolio,
+    websiteLink: "https://armandosportfolio.netlify.app/",
+    githubLink: "https://github.com/armando12100/Portfolio",
+  },
+  {
+    title: "FakeFlix",
+    id: 3,
+    paragraph: "Netflix Clone",
+    video: fakeflix,
+    websiteLink: "https://bucolic-sherbet-380004.netlify.app/",
+    githubLink: "https://github.com/armando12100/netflix_clone",
+  },
+];
